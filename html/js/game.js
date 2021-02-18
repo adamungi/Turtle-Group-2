fdd78051dd8f7c63212d7b0e111d4dbad317fc68
adamungi <71020027+adamungi@users.noreply.github.com>
2021-02-18 15:23:13 +0000

    Update game.js
    added a player element

--- html/js/game.js
+++ html/js/game.js
@@ -2,3 +2,15 @@ const context = document.querySelector("canvas").getContext("2d");
 
 context.canvas.height = 400;
 context.canvas.width = 1220;
+
+var character = new character();
+
+const square = {
+  height: 32,
+  jumping: true,
+  width: 32,
+  x: 0,
+  xVelocity: 0,
+  y: 0,
+  yVelocity: 0
+};
